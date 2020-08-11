a556000933cc8a1354ccb2de2aa4f9b6f26dcd62
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-11 15:37:34 -0500

    linking javascript

---
+++ index.js
@@ -0,0 +1,3 @@
+//when i start the quiz I need to make jumbotron disappear and quiz start
+//need to install timer inside navbar
+//need to store initials and score in local storage and be able to clear it
