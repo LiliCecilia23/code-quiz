8d921400ee8576c998686987b8a8391f56755b41
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-12 14:54:44 -0500

    moved questions array into main js file, made functions to control appearance of elements

--- index.js
+++ index.js
@@ -5,9 +5,33 @@
 
 // globl variables
 let correct = [];
+let quizDiv = document.getElementById("quiz");
+let jumbotron = document.getElementById("quizstart");
+let questionsArray = [
+    {question1: "Commonly used data types DO NOT include:",
+     answers1: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+     correct: "3. alerts"},
+    {question2: "The condition in an if / else statement is enclosed within _____.",
+     answers2: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+     correct: "3. parentheses"},
+    {question3: "Arrays in JavaScript can be used to store _____.",
+     answers3: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+     correct: "4. all of the above"},
+    {question4: "String values must be enclosed within _____ when being assigned to variables.",
+     answers4: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+     correct: "3. quotes"},
+    {question5: "A very useful tool used during development and debugging for printing content to the debugger is:",
+     answers5: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+     correct: "4. console.log"},
+]
 
+
+//hides div that contains the questions on the home page
+function quizHide (){
+    quizDiv.style.display = "none"
+}
+quizHide();
 // makes jumbotron element disappear when quiz is started
-var jumbotron = document.getElementById("quizstart");
 document.getElementById("startButton").addEventListener("click", function (){
     jumbotron.style.display = "none";
     window.setInterval(timer, 1000)
@@ -25,13 +49,12 @@ function timer() {
 
 // makes quiz questions appear
 function quizQuestions (){
+    quizDiv.style.display = "block";
+
     for (var i = 0; i < questionsArray.length; i++){
         var qHeading = document.getElementById("questionHeader");
-        qHeading.textContent = "questionsArray[i][i]";
+        qHeading.innerHTML = questionsArray[i][i];
 
-        // var qHeading = document.createElement("div");
-        // qHeading.innerHTML = "questionsArray[i][i]";
-        // document.getElementById("questionel").appendChild(qHeading);
     }
 }
 
