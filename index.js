9becb5c2e0b8bb12bab8354cdb409a3b85e08b6a
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-13 13:56:44 -0400

    adding button functionality and made a checking function

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // prevent default
 
 // globl variables
-let correct = [];
 let quizDiv = document.getElementById("quiz");
 let quizAnswer1 = document.getElementById("answer1");
 let quizAnswer2 = document.getElementById("answer2");
@@ -14,20 +13,22 @@ let jumbotron = document.getElementById("quizstart");
 let questionsArray = [
     {question: "Commonly used data types DO NOT include:",
      answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-     correct: "3. alerts"},
+     correctAnswer: "3. alerts"},
     {question: "The condition in an if / else statement is enclosed within _____.",
      answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-     correct: "3. parentheses"},
+     correctAnswer: "3. parentheses"},
     {question: "Arrays in JavaScript can be used to store _____.",
      answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-     correct: "4. all of the above"},
+     correctAnswer: "4. all of the above"},
     {question: "String values must be enclosed within _____ when being assigned to variables.",
      answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-     correct: "3. quotes"},
+     correctAnswer: "3. quotes"},
     {question: "A very useful tool used during development and debugging for printing content to the debugger is:",
      answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
-     correct: "4. console.log"},
-]
+     correctAnswer: "4. console.log"},
+];
+let score = 0;
+let displayedQuestion = 0;
 
 
 //hides div that contains the questions on the home page
@@ -35,6 +36,7 @@ function quizHide (){
     quizDiv.style.display = "none"
 }
 quizHide();
+
 // makes jumbotron element disappear when quiz is started
 document.getElementById("startButton").addEventListener("click", function (){
     jumbotron.style.display = "none";
@@ -49,27 +51,80 @@ var startTime = 90;
 function timer() {
     startTime--;
     document.getElementById("timerElement").innerHTML = startTime;
+    if (startTime === 0 || questionsArray.length === displayedQuestion) {
+        clearInterval(timer);
+        quizFinish();
+    }
 }
 
 // makes quiz questions appear
 function quizQuestions (){
+    //makes quiz appear
     quizDiv.style.display = "block";
+    
+    //sets text of the question
+    var qHeading = document.getElementById("questionHeader");
+    qHeading.innerHTML = questionsArray[displayedQuestion].question;
 
-    for (var i = 0; i < questionsArray.length; i++){
-        var qHeading = document.getElementById("questionHeader");
-        qHeading.innerHTML = questionsArray[i].question;
+    //sets text of answer choices
+    quizAnswer1.innerHTML = questionsArray[displayedQuestion].answers[0];
+    quizAnswer2.innerHTML = questionsArray[displayedQuestion].answers[1];
+    quizAnswer3.innerHTML = questionsArray[displayedQuestion].answers[2];
+    quizAnswer4.innerHTML = questionsArray[displayedQuestion].answers[3];
+      
+    //makes answer buttons functional
+    quizAnswer1.addEventListener("click", function (event){
+        check(event);
+    })
+    quizAnswer2.addEventListener("click", function (event){
+        check(event);
+    })
+    quizAnswer3.addEventListener("click", function (event){
+        check(event);
+    })
+    quizAnswer4.addEventListener("click", function (event){
+        check(event);
+    })
+}
 
-       quizAnswer1.innerHTML = questionsArray[i].answers[0];
-       quizAnswer2.innerHTML = questionsArray[i].answers[1];
-       quizAnswer3.innerHTML = questionsArray[i].answers[2];
-       quizAnswer4.innerHTML = questionsArray[i].answers[3];
+//creates answer choice feedback
+var result = document.createElement("p");
+document.getElementById("quiz").appendChild(result);
 
+//checks answer choice
+function check (event){
+    var choice = event.currentTarget.dataset.answers;
+    var correctChoice = null;
+
+    // if they choose correctly, give them points
+    if (questionsArray[displayedQuestion].correctAnswer === choice){
+        correctChoice = choice;
+    }
+    if (choice === correctChoice){
+        result.innerHTML = "Correct answer!";
+        score += 10; 
+    } else { //if they choose incorrectly, deduct points and time left on the timer
+        result.innerHTML = "Wrong answer!";
+        score -= 10;
+        startTime -= 10;
+        if (startTime < 0) {
+            startTime = 0;
+        }
+    }
+    // if they've gone through all the questions, make the quiz stop and input high score
+    if (questionsArray.length - 1 === displayedQuestion){
+        quizFinish();
+        return;
+    } else { //if there's more questions, keep going
+        displayedQuestion++;
+        quizQuestions();
     }
 }
 
-// sets what happens when an answer is wrong
+function quizFinish (){
+    quizHide ();
+    
+}
 
 
-
-// sets what happens when an answer is right
 
