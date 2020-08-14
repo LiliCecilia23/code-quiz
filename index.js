4df604a82124c37ce4c623b2c30d164b3b69471d
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-13 21:47:20 -0400

    building highscore page

--- index.js
+++ index.js
@@ -4,45 +4,46 @@
 // prevent default
 
 // globl variables
-let quizDiv = document.getElementById("quiz");
-let quizAnswer1 = document.getElementById("answer1");
-let quizAnswer2 = document.getElementById("answer2");
-let quizAnswer3 = document.getElementById("answer3");
-let quizAnswer4 = document.getElementById("answer4");
-let jumbotron = document.getElementById("quizstart");
-let questionsArray = [
+var quizDiv = document.getElementById("quiz");
+var quizAnswer1 = document.getElementById("answer1");
+var quizAnswer2 = document.getElementById("answer2");
+var quizAnswer3 = document.getElementById("answer3");
+var quizAnswer4 = document.getElementById("answer4");
+var jumbotron = document.getElementById("quizstart");
+var questionsArray = [
     {question: "Commonly used data types DO NOT include:",
      answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-     correctAnswer: "3. alerts"},
+    },
     {question: "The condition in an if / else statement is enclosed within _____.",
      answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-     correctAnswer: "3. parentheses"},
+    },
     {question: "Arrays in JavaScript can be used to store _____.",
      answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-     correctAnswer: "4. all of the above"},
+    },
     {question: "String values must be enclosed within _____ when being assigned to variables.",
      answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-     correctAnswer: "3. quotes"},
+    },
     {question: "A very useful tool used during development and debugging for printing content to the debugger is:",
      answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
-     correctAnswer: "4. console.log"},
+    }
 ];
-let score = 0;
-let displayedQuestion = 0;
+var correctAnswers = ["3. alerts","3. parentheses", "4. all of the above", "3. quotes", "4. console.log"];
+var score = 0;
+var displayedQuestion = 0;
 
 
 //hides div that contains the questions on the home page
 function quizHide (){
-    quizDiv.style.display = "none"
+    quizDiv.style.display = "none";
 }
 quizHide();
 
 // makes jumbotron element disappear when quiz is started
 document.getElementById("startButton").addEventListener("click", function (){
     jumbotron.style.display = "none";
-    window.setInterval(timer, 1000)
+    window.setInterval(timer, 1000);
     quizQuestions();
-})
+});
 
 // sets the start time
 var startTime = 90;
@@ -75,16 +76,16 @@ function quizQuestions (){
     //makes answer buttons functional
     quizAnswer1.addEventListener("click", function (event){
         check(event);
-    })
+    });
     quizAnswer2.addEventListener("click", function (event){
         check(event);
-    })
+    });
     quizAnswer3.addEventListener("click", function (event){
         check(event);
-    })
+    });
     quizAnswer4.addEventListener("click", function (event){
         check(event);
-    })
+    });
 }
 
 //creates answer choice feedback
@@ -93,26 +94,24 @@ document.getElementById("quiz").appendChild(result);
 
 //checks answer choice
 function check (event){
-    var choice = event.currentTarget.dataset.answers;
-    var correctChoice = null;
+    var choice = event.currentTarget.innerHTML;
 
     // if they choose correctly, give them points
-    if (questionsArray[displayedQuestion].correctAnswer === choice){
-        correctChoice = choice;
-    }
-    if (choice === correctChoice){
+    if (correctAnswers[displayedQuestion] === choice){
         result.innerHTML = "Correct answer!";
         score += 10; 
     } else { //if they choose incorrectly, deduct points and time left on the timer
         result.innerHTML = "Wrong answer!";
         score -= 10;
-        startTime -= 10;
         if (startTime < 0) {
             startTime = 0;
+            clearInterval(timer);
+        } else {
+            startTime -= 10;
         }
     }
     // if they've gone through all the questions, make the quiz stop and input high score
-    if (questionsArray.length - 1 === displayedQuestion){
+    if (questionsArray.length === (displayedQuestion + 1)){
         quizFinish();
         return;
     } else { //if there's more questions, keep going
@@ -123,7 +122,7 @@ function check (event){
 
 function quizFinish (){
     quizHide ();
-    
+    window.location.href="./highscore.html"
 }
 
 
