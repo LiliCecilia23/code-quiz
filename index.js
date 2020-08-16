944c4ea59b5dea6970028548f8d93bb3b2500ec0
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-16 00:03:27 -0400

    actually got quiz to work and itterate properly

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ var questionsArray = [
      answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
     },
     {question: "The condition in an if / else statement is enclosed within _____.",
-     answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"]
+     answers: ["1. quotes", "2. parentheses", "3. curly brackets", "4. square brackets"]
     },
     {question: "Arrays in JavaScript can be used to store _____.",
      answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]
     },
     {question: "String values must be enclosed within _____ when being assigned to variables.",
-     answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"]
+     answers: ["1. quotes", "2. curly brackets", "3. commas", "4. parentheses"]
     },
     {question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-     answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"]
+     answers: ["1. JavaScript", "2. terminal/bash", "3. console.log", "4. for loops"]
     }
 ];
-var correctAnswers = ["3. alerts","3. parentheses", "4. all of the above", "3. quotes", "4. console.log"];
+var correctAnswers = ["3. alerts","2. parentheses", "4. all of the above", "1. quotes", "3. console.log"];
 var score = 0;
 var displayedQuestion = 0;
 
@@ -76,51 +76,31 @@ function quizQuestions (){
     quizAnswer2.innerHTML = questionsArray[displayedQuestion].answers[1];
     quizAnswer3.innerHTML = questionsArray[displayedQuestion].answers[2];
     quizAnswer4.innerHTML = questionsArray[displayedQuestion].answers[3];
-     
-    //makes answer buttons functional
-    quizAnswer1.addEventListener("click", function (event){
-        check(event);
-    });
-    quizAnswer2.addEventListener("click", function (event){
-        check(event);
-    });
-    quizAnswer3.addEventListener("click", function (event){
-        check(event);
-    });
-    quizAnswer4.addEventListener("click", function (event){
-        check(event);
-    });
+    
+    if (displayedQuestion === 5){
+        quizFinish();
+    }
 }
 
 //creates answer choice feedback
 var result = document.createElement("p");
 document.getElementById("quiz").appendChild(result);
 
-//checks answer choice
-function check (event){
-    var choice = event.currentTarget.innerHTML;
-    
-    // if they choose correctly, give them points
-    if (correctAnswers[displayedQuestion] === choice){
-        result.innerHTML = "Correct answer!";
-        score += 10; 
-    } else { //if they choose incorrectly, deduct points and time left on the timer
-        result.innerHTML = "Wrong answer!";
-        score -= 10;
-        if (startTime < 0) {
-            startTime = 0;
-            clearInterval(interval);
-        } else {
-            startTime -= 10;
-        }
-    }
-    // if they've gone through all the questions, make the quiz stop and input high score
-    if (questionsArray.length === (displayedQuestion)){
-        quizFinish();
-        return;
-    } else { //if there's more questions, keep going
-        displayedQuestion++;
-        quizQuestions();
+function correct (){
+    result.innerHTML = "Correct answer!";
+    score += 10; 
+    displayedQuestion++;
+    quizQuestions();
+}    
+
+function wrong (){
+    result.innerHTML = "Wrong answer!";
+    score -= 10;
+    if (startTime < 0) {
+        startTime = 0;
+        clearInterval(interval);
+    } else {
+        startTime -= 10;
     }
 }
 
@@ -131,3 +111,36 @@ function quizFinish (){
     currentScore.appendChild(yourScore);
     yourScore.innerHTML = score;
 }
+
+//makes answer buttons functional
+quizAnswer1.addEventListener("click", function (){
+    if (displayedQuestion === 3){
+        correct();
+    } else {
+        wrong();
+    }
+});
+quizAnswer2.addEventListener("click", function (){
+   if (displayedQuestion === 1){
+        correct();
+    } else {
+        wrong();
+    }
+});
+quizAnswer3.addEventListener("click", function (){
+   if (displayedQuestion === 0){
+        correct();
+    } else if (displayedQuestion === 4){
+        score += 10;
+        quizFinish();
+    } else {
+        wrong();
+    }
+});
+quizAnswer4.addEventListener("click", function (){
+   if (displayedQuestion === 2){
+        correct();
+    } else {
+        wrong();
+    }
+});
